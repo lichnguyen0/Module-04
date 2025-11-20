4b1eb57b7999ff36a2540c0a96dee7de08ce4d4b
lichnguyen0 <fakesangmy@gmail.com>
2025-11-20 17:08:47 +0700

    update

---
+++ 4. Data Binding & Form/md4-b4-th2/src/main/java/com/example/md4b4th2/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.md4b4th2.controller;
+
+import com.example.md4b4th2.dao.UserDao;
+import com.example.md4b4th2.model.Login;
+import com.example.md4b4th2.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("login", new Login());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDao.checkLogin(login);
+        ModelAndView modelAndView;
+        if(user == null){
+            modelAndView = new ModelAndView("error");
+        } else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+        }
+        return modelAndView;
+    }
+}
