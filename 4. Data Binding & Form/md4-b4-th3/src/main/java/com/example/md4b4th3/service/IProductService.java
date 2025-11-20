a10dce1103231c36b5af3373e2d3de050729e32b
lichnguyen0 <fakesangmy@gmail.com>
2025-11-20 17:48:38 +0700

    update

---
+++ 4. Data Binding & Form/md4-b4-th3/src/main/java/com/example/md4b4th3/service/IProductService.java
@@ -0,0 +1,17 @@
+package com.example.md4b4th3.service;
+
+import com.example.md4b4th3.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+}
