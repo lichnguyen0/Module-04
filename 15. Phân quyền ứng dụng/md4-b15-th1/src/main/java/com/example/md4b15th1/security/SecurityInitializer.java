4d90f0313ebf12603ec30e1c2686b114dd3d0a07
lichnguyen0 <fakesangmy@gmail.com>
2025-12-08 14:27:50 +0700

    update

---
+++ 15. Phân quyền ứng dụng/md4-b15-th1/src/main/java/com/example/md4b15th1/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.example.md4b15th1.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
