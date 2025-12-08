12ee7337a1dc503c4e0edda31fbcd884fbc2b7e0
lichnguyen0 <fakesangmy@gmail.com>
2025-12-08 15:30:56 +0700

    update

---
+++ 15. Phân quyền ứng dụng/md4-b15-th2/src/main/java/com/example/md4b15th2/configuration/AppConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.md4b15th2.configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example")
+public class AppConfiguration implements WebMvcConfigurer {
+}
---
+++ 15. Phân quyền ứng dụng/md4-b15-th2/src/main/java/com/example/md4b15th2/jwt/config/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.example.md4b15th2.jwt.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
