172cdc0288cf43855bd6a92b8f8fa61987b8dec7
lichnguyen0 <fakesangmy@gmail.com>
2025-12-10 11:18:16 +0700

    update

---
+++ 17. Spring Boot/md4-b17-th1/src/main/java/com/example/md4b17th1/Md4B17Th1Application.java
@@ -0,0 +1,13 @@
+package com.example.md4b17th1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4B17Th1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4B17Th1Application.class, args);
+    }
+
+}
---
+++ 17. Spring Boot/md4-b17-th1/src/test/java/com/example/md4b17th1/Md4B17Th1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md4b17th1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4B17Th1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
