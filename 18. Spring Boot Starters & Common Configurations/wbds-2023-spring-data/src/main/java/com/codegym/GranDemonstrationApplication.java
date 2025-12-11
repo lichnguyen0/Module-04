a4861480d2713b4661d7488e22a209d4ddc8dfbf
lichnguyen0 <fakesangmy@gmail.com>
2025-12-11 17:04:28 +0700

    update

---
+++ 18. Spring Boot Starters & Common Configurations/wbds-2023-spring-data/src/main/java/com/codegym/GranDemonstrationApplication.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranDemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranDemonstrationApplication.class, args);
+    }
+}
